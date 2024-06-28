ee696e5c859f9434dd956d5a469b86c0079b66f6
Clarence <clarencedo@126.com>
2024-06-28 16:04:04 +0800

    nvim config init

---
+++ nvim/lua/plugins/clarence.lua
@@ -0,0 +1,134 @@
+return {
+	{
+		"tzachar/cmp-tabnine",
+		build = "./install.sh",
+		dependencies = "hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+	},
+	-- {
+	-- 	"github/copilot.vim",
+	-- 	event = "InsertEnter",
+	-- 	config = function()
+	-- 		vim.g.copilot_no_tab_map = true
+	-- 		vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+	-- 	end,
+	-- },
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
+	{
+		url = "https://codeberg.org/jthvai/lavender.nvim",
+		branch = "stable", -- versioned tags + docs updates from main
+		lazy = false,
+		priority = 1000,
+	},
+
+	{
+		"rmagatti/goto-preview",
+		config = function()
+			require("goto-preview").setup({
+				width = 150, -- Width of the floating window
+				height = 40, -- Height of the floating window
+				default_mappings = true, -- Bind default mappings
+				debug = false, -- Print debug information
+				opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
+				post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
+				-- You can use "default_mappings = true" setup option
+				-- Or explicitly set keybindings
+				-- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+				-- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+				-- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+			})
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+				throttle = true, -- Throttles plugin updates (may improve performance)
+				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+				patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+					-- For all filetypes
+					-- Note that setting an entry here replaces all other patterns for this entry.
+					-- By setting the 'default' entry below, you can control which nodes you want to
+					-- appear in the context window.
+					default = {
+						"class",
+						"function",
+						"method",
+					},
+				},
+			})
+		end,
+	},
+
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+
+	{
+		"jackMort/ChatGPT.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("chatgpt").setup({
+				api_key_cmd = "echo 'OPENAI_API_KEY'",
+			})
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"folke/trouble.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+
+	{
+		"itchyny/vim-cursorword",
+		event = { "BufEnter", "BufNewFile" },
+		config = function()
+			vim.api.nvim_command("augroup user_plugin_cursorword")
+			vim.api.nvim_command("autocmd!")
+			vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
+			vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
+			vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
+			vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
+			vim.api.nvim_command("augroup END")
+		end,
+	},
+	-- database client
+	{
+		"kndndrj/nvim-dbee",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		build = function()
+			-- Install tries to automatically detect the install method.
+			-- if it fails, try calling it with one of these parameters:
+			--    "curl", "wget", "bitsadmin", "go"
+			require("dbee").install()
+		end,
+		config = function()
+			require("dbee").setup(--[[optional config]])
+		end,
+	},
+	-- 添加 EditorConfig 插件
+	-- {
+	--   "editorconfig/editorconfig-vim",
+	--   config = function()
+	--     -- 插件加载后的配置（如果需要）
+	--   end,
+	-- },
+	-- {
+	--   "Pocco81/auto-save.nvim",
+	--   config = function()
+	--     require("auto-save").setup()
+	--   end,
+	-- },
+}
